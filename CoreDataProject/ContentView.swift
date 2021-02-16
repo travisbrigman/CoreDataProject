d8b052ce2a3a0a6a344742cdc71d347667147964
travisbrigman <travis1000@gmail.com>
2021-02-16 06:24:58 -0600

    Initial Commit

---
+++ CoreDataProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Travis Brigman on 2/16/21.
+//  Copyright © 2021 Travis Brigman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
